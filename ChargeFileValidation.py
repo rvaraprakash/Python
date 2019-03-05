9d8e2e4261e9e27dee32fba1851bbdbb1f4298c9
rvaraprakash <35334785+rvaraprakash@users.noreply.github.com>
2019-03-04 22:07:01 -0500

    Add files via upload
    Changes for crate filenames

--- ChargeFileValidation.py
+++ ChargeFileValidation.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 import pandas as pd
+from datetime import datetime
 
-df = pd.read_csv('C:\Vara\AM&R\scripts\Ref_Scripts\ChargeFileGen\BL_RATED.csv')
+filename="C:\Vara\AM&R\scripts\Ref_Scripts\ChargeFileGen\BL_RATED.csv"
+# df = pd.read_csv('C:\Vara\AM&R\scripts\Ref_Scripts\ChargeFileGen\BL_RATED.csv')
+df = pd.read_csv(filename)
 
 clean_df = df[df['AR_ROUNDED_PRICE'] > 0]
 
@@ -18,13 +21,26 @@ PRIMDEV_DIV = ['NYC']
 CHRG_KEYS = ['DIVISION_CODE', 'ACCOUNT_TYPE', 'SERVICE_TYPE', 'SERVICE_TYPE', 'AR_ROUNDED_PRICE', 'FINANCE_ENTITY']
 ACC_SERV_KEYS = ['ACCOUNT_TYPE', 'SERVICE_TYPE']
 
+#### Create file
+def createFile_ICOMS(*args):
+    print(args)
+
+
 #### PRI Accounts
 priAcc_df = clean_df[clean_df['DIVISION_CODE'].isin(PRI_DIV) & clean_df['ACCOUNT_TYPE'].isin(['C', 'T'])
                      & clean_df['SERVICE_TYPE'].isin(['T'])]
 if (len(priAcc_df)):
     print("PRI Accounts")
     print(priAcc_df[CHRG_KEYS])
+    row_df = priAcc_df.filter(['FINANCE_ENTITY','CREDIT_DEBIT_IND','SERVICE_TYPE','USAGE_CYCLE_END'])
 
+    row_df['fileTime'] = pd.to_datetime(row_df['USAGE_CYCLE_END'])
+    row_df['fileTime'] = row_df.fileTime.apply(lambda x: datetime.strftime(x, '%Y%m%d'))
+    row_df.drop_duplicates(inplace=True)
+    
+    print(row_df)
+
+"""
 #### RES Accounts
 resAcc_df = clean_df[clean_df['DIVISION_CODE'].isin(RES_DIV) & clean_df['ACCOUNT_TYPE'].isin(['R'])
                      & clean_df['SERVICE_TYPE'].isin(['R'])]
@@ -93,3 +109,5 @@ if (len(bhnComAcc_df)):
 # df2= df1.groupby(['ACCOUNT_NUMBER','CHARGE_NUMBER'])['AR_ROUNDED_PRICE'].sum()
 
 # print(df2)
+
+"""
