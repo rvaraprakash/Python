ff7ef70c4a7e77ec6b3a7e24007fead5115098e2
rvaraprakash <35334785+rvaraprakash@users.noreply.github.com>
2019-03-06 13:26:36 -0500

    Update help.py

--- help.py
+++ help.py
@@ -12,3 +12,10 @@ finally:
   print("Resource closed")
   
   https://www.martymusic.com
+    
+  ### Updating dataframe column based on other column
+  ## Example update CREDIT_DEBIT_IND = D if CALL_TYPE=LD2
+  tmp_df.loc[tmp_df['CALL_TYPE'] == 'LD2', ['CREDIT_DEBIT_IND']]='D'
+  
+  ## Update CREDIT_DEBIT_IND, TAX_INCLUSIVE_IND if CALL_TYPE=LD2
+  tmp_df.loc[tmp_df['CALL_TYPE'] == 'LD2', ['CREDIT_DEBIT_IND', 'TAX_INCLUSIVE_IND']]=['D', 0]
